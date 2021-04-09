c915f91b2d62a2382acaf503148b1304a7a040d7
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2021-04-08 22:35:31 -0500

    Conectando cliente servidor io

--- index.js
+++ index.js
@@ -15,9 +15,18 @@ io.on('connection', client => {
 
     console.log('Cliente conectado')
     
+
     client.on('disconnect', () => { 
         console.log('Cliente Desconectado')
      });
+
+
+     client.on('mensaje',( payload )=>{
+        console.log('Mensaje!!!',payload);
+        io.emit('mensaje',{admin:'Nuevo Mensaje'});
+
+     })
+
   });
 
 
