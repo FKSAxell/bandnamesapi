5f4cb127f6b7218756f87a12e11db424a6dba648
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2021-04-09 23:11:32 -0500

    seccion 30

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,19 +1,19 @@
 // Mensajes de Sockets
-const {io} = require('../index');
-io.on('connection', client => {
+const { io } = require("../index");
+io.on("connection", (client) => {
+  console.log("Cliente conectado");
 
-    console.log('Cliente conectado')
-    
-
-    client.on('disconnect', () => { 
-        console.log('Cliente Desconectado')
-     });
-
-
-     client.on('mensaje',( payload )=>{
-        console.log('Mensaje!!!',payload);
-        io.emit('mensaje',{admin:'Nuevo Mensaje'});
-
-     })
-
+  client.on("disconnect", () => {
+    console.log("Cliente Desconectado");
   });
+
+  client.on("mensaje", (payload) => {
+    console.log("Mensaje!!!", payload);
+    io.emit("mensaje", { admin: "Nuevo Mensaje" });
+  });
+
+  client.on("emitir-mensaje", (payload) => {
+    //io.emit("nuevo-mensaje", payload); // a todos los clientes
+    client.broadcast.emit("nuevo-mensaje", payload); // emite a todos menos al que lo emitiio
+  });
+});
